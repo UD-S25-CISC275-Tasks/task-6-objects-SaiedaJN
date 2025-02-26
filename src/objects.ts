1b0f704d42cf6a52e1f4c7150a9769743f8ea22e
saieda <saieda@udel.edu>
2025-02-26 13:07:20 -0500

    func 4,5,6,7 are passed.

--- src/objects.ts
+++ src/objects.ts
@@ -46,7 +46,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return `${question.id}: ${question.name.slice(0,10)}`;
 }
 
 /**
@@ -67,7 +67,22 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+
+    // let result = `#  ${question.name}\n${question.body}`;
+
+    // if (question.type === "multiple_choice_question"){
+    //     result += '\n' + question.options.map(option => `- ${option}`).join("\n");
+    // }
+
+    // return result;
+
+    let result = `# ${question.name}\n${question.body}`;
+
+    if (question.type === "multiple_choice_question") {
+        result += `\n` + question.options.map(option => `- ${option}`).join("\n");
+    }
+
+    return result;
 }
 
 /**
@@ -75,7 +90,8 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return { ...question, name:newName };
+
 }
 
 /**
@@ -84,7 +100,7 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return {...question, published:!question.published};
 }
 
 /**
@@ -94,7 +110,8 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    
+    return {...oldQuestion, id:id, name:`Copy of ${oldQuestion.name}`, published: false};
 }
 
 /**
